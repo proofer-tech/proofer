4b56ae19d1c6ecc28475f10331397d186f342e73
Hansol-Lim <dev.hansollim@gmail.com>
2024-02-16 02:24:11 +0900

    Add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "@/theme";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   metadataBase: new URL("https://proofer.tech"),
@@ -37,6 +38,7 @@ export default function RootLayout({ children }: { children: any }) {
       <body>
         <MantineProvider theme={theme}>{children}</MantineProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
