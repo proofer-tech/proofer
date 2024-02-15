025bb24c5232148f64aa6cf1450fadc0eb8757e1
Hansol-Lim <dev.hansollim@gmail.com>
2024-02-16 01:39:02 +0900

    fix for gcp

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "@/theme";
 
 export const metadata = {
+  metadataBase: new URL("https://proofer.tech"),
   title: "프루퍼 | 개발자 성과측정 SaaS",
   description:
     "정확한 개발자 성과측정을 위해 실리콘 밸리 빅테크기업들에서 검증된 프레임워크를 기반한 플랫폼을 제공하는 엔지니어링 매니징 파트너",
