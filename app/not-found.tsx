47047d0b1619157f64637618d8bc7eb7c89c4771
Hansol-Lim <dev.hansollim@gmail.com>
2024-02-19 16:22:54 +0900

    window not defined

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,9 +3,15 @@ import { redirect } from "next/navigation";
 
 export default function NotFound() {
   // FIXME: 404 일 경우 medium 이 있는지를 판별하여 이동하도록 변경
-  if (window !== undefined)
+  try {
     return redirect(
       `https://medium.com/@proofer.tech${window.location.pathname}`,
     );
-  else return redirect("/");
+  } catch (e) {
+    if (e instanceof ReferenceError) {
+      return redirect("/");
+    }
+
+    throw e;
+  }
 }
