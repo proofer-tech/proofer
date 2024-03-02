3755bb4a0d5d2a76be4c1c13d51384cfdb5b4f44
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-02 19:15:19 +0900

    feat: prisma 세팅 및 Health 페이지 신설

---
+++ app/health/page.tsx
@@ -0,0 +1,193 @@
+"use client";
+import { useDisclosure } from "@mantine/hooks";
+import useTallyInquireForm from "@/hooks/tally";
+import LandingPageShell from "@/app/components/LandingPageShell";
+import {
+  AppShell,
+  Badge,
+  Center,
+  Container,
+  Group,
+  Loader,
+  Skeleton,
+  Stack,
+  Table,
+  Text,
+  Title,
+  Tooltip,
+} from "@mantine/core";
+import {
+  InquireCompletedModal,
+  NotReadyYetModal,
+} from "@/app/components/Modal";
+import Footer from "@/app/components/Footer";
+import React from "react";
+import Header from "@/app/components/Header";
+import {
+  IconAlertCircleFilled,
+  IconCircleCheckFilled,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import useSWR from "swr";
+import { $Enums, Health } from "@prisma/client";
+import { apiFetcher } from "@/app/src/swr";
+import HealthState = $Enums.HealthState;
+
+const HealthStateLabel = {
+  [HealthState.UP]: "운영중",
+  [HealthState.DOWN]: "확인필요",
+  [HealthState.MAINTENANCE]: "작업중",
+};
+
+const HealthStateColor = {
+  [HealthState.UP]: "var(--color-green)",
+  [HealthState.DOWN]: "var(--color-red)",
+  [HealthState.MAINTENANCE]: "var(--color-primary)",
+};
+
+export default function HealthPage() {
+  const navbarDisclosure = useDisclosure(false);
+
+  const [isInquireCompletedModalOpened, inquireCompletedModal] =
+    useDisclosure(false);
+  const [notReadyYetModalOpened, notReadyYetModal] = useDisclosure(false);
+  const { openTallyPopup } = useTallyInquireForm({
+    onSubmit: () => inquireCompletedModal.open(),
+  });
+
+  const { data, error, isLoading } = useSWR<Health[]>(
+    "/api/health",
+    apiFetcher,
+  );
+
+  return (
+    <LandingPageShell isNavbarOpened={navbarDisclosure[0]}>
+      <Header
+        isNavbarOpened={navbarDisclosure[0]}
+        onBurgerClick={navbarDisclosure[1].toggle}
+        onLoginClick={() => notReadyYetModal.open()}
+        onInquireClick={() => openTallyPopup()}
+      />
+      <AppShell.Main>
+        <Container>
+          <Center pt={"5vh"}>
+            {isLoading ? (
+              <Loader color="rgba(0, 0, 0, 0.1)" size={"9em"} />
+            ) : error ||
+              (data && data.some((h) => h.state === HealthState.DOWN)) ? (
+              <IconAlertCircleFilled
+                size={"10em"}
+                style={{ color: "var(--color-red)" }}
+              />
+            ) : (
+              <IconCircleCheckFilled
+                size={"10em"}
+                style={{ color: "var(--color-green)" }}
+              />
+            )}
+          </Center>
+          <Stack py={"3em"} align={"center"}>
+            {isLoading ? (
+              <Skeleton width={"80%"} height={"2em"} radius="xl" />
+            ) : error ||
+              (data && data.some((h) => h.state === HealthState.DOWN)) ? (
+              <Title order={1}>서비스에 문제를 발견하여 확인중입니다.</Title>
+            ) : (
+              <Title order={1}>프루퍼 서비스가 정상 동작중입니다.</Title>
+            )}
+
+            {isLoading ? (
+              <Skeleton width={"90%"} height={"1em"} radius="xl" />
+            ) : data && data.every((h) => h.state !== HealthState.DOWN) ? (
+              <Text>
+                혹시 문제를 겪고 계시나요? 문제상황을{" "}
+                <Link href={"mailto:info@campersground.kr"}>
+                  info@campersground.kr
+                </Link>{" "}
+                으로 전달해주시면 빠르게 확인 후 답변해드리겠습니다!
+              </Text>
+            ) : (
+              <Text>
+                최대한 빠르게 복구하겠습니다. 양해해주셔서 감사합니다!
+              </Text>
+            )}
+          </Stack>
+          <Group gap={"1em"} justify={"center"} py={"2em"}>
+            {isLoading
+              ? Array.from({ length: 3 }, () => (
+                  <Skeleton width={"10em"} height={"2em"} radius="xl" />
+                ))
+              : data &&
+                data.map((h) => (
+                  <Tooltip.Floating label={h.description}>
+                    <Badge
+                      key={h.id}
+                      size="xl"
+                      variant="dot"
+                      color={(() => {
+                        switch (h.state) {
+                          case HealthState.UP:
+                            return "var(--color-green)";
+                          case HealthState.DOWN:
+                            return "var(--color-red)";
+                          case HealthState.MAINTENANCE:
+                            return "var(--color-primary)";
+                          default:
+                            return "var(--color-darkgray-2)";
+                        }
+                      })()}
+                    >
+                      {h.name}
+                    </Badge>
+                  </Tooltip.Floating>
+                ))}
+          </Group>
+          {isLoading ? (
+            <Stack>
+              {Array.from({ length: 5 }, () => (
+                <Skeleton width={"100%"} height={"1em"} />
+              ))}
+            </Stack>
+          ) : (
+            data &&
+            data.some((h) => h.description !== null) && (
+              <Table captionSide="bottom">
+                {data
+                  .filter((h) => h.description !== null)
+                  .map((h) => (
+                    <Table.Tr key={h.id}>
+                      <Table.Td>
+                        <Badge
+                          variant="light"
+                          color={HealthStateColor[h.state]}
+                        >
+                          {HealthStateLabel[h.state]}
+                        </Badge>
+                      </Table.Td>
+                      <Table.Td>{h.name}</Table.Td>
+                      <Table.Td>{h.description}</Table.Td>
+                    </Table.Tr>
+                  ))}
+              </Table>
+            )
+          )}
+        </Container>
+      </AppShell.Main>
+      <NotReadyYetModal
+        isOpened={notReadyYetModalOpened}
+        onCloseClick={notReadyYetModal.close}
+      />
+      <InquireCompletedModal
+        isOpened={isInquireCompletedModalOpened}
+        onCloseClick={inquireCompletedModal.close}
+      />
+      <AppShell.Footer
+        pos={"static"}
+        bg={"transparent"}
+        style={{ border: "none" }}
+      >
+        <Footer />
+      </AppShell.Footer>
+    </LandingPageShell>
+  );
+}
