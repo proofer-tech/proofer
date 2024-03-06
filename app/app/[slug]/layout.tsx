5e2d90f23cb647b92996bb3a874c62421c8d0446
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-06 18:51:00 +0900

    Add subpage routing and update main page layout

--- app/app/[slug]/layout.tsx
+++ app/app/[slug]/layout.tsx
@@ -18,31 +18,38 @@ export default function MainLayout({ children }: { children: any }) {
   const pathname = usePathname();
   const pathBlocks = pathname.split("/").slice(2);
   const path = pathTree[pathBlocks[0]];
-  const subPath = path.subTree![pathBlocks[1]];
-  return (
-    <Stack>
+  const subPath = path && path.subTree && path.subTree[pathBlocks[1]];
+
+  // sub-page
+  if (subPath !== undefined)
+    return (
       <Stack>
-        <Breadcrumbs separator={"-"}>
-          <Group gap={"1ex"}>
-            {path.tablerIcon && (
-              <Center
-                p={"0.3em"}
-                bg={"var(--mantine-color-gray-2)"}
-                style={{ borderRadius: "4px" }}
-              >
-                <path.tablerIcon
-                  size={"1em"}
-                  color={"var(--mantine-color-gray-8)"}
-                />
-              </Center>
-            )}
-            <Text>{path.title}</Text>
-          </Group>
-        </Breadcrumbs>
-        <Title order={1}>{subPath.title}</Title>
+        <Stack>
+          <Breadcrumbs separator={"-"}>
+            <Group gap={"1ex"} wrap={"nowrap"}>
+              {path.tablerIcon && (
+                <Center
+                  p={"0.3em"}
+                  bg={"var(--mantine-color-gray-2)"}
+                  style={{ borderRadius: "4px" }}
+                >
+                  <path.tablerIcon
+                    size={"1em"}
+                    color={"var(--mantine-color-gray-8)"}
+                  />
+                </Center>
+              )}
+              <Text>{path.title}</Text>
+            </Group>
+          </Breadcrumbs>
+          <Title order={1} style={{ whiteSpace: "nowrap" }}>
+            {subPath.title}
+          </Title>
+        </Stack>
+        <Divider w={"100%"} />
+        <Box>{children}</Box>
       </Stack>
-      <Divider w={"100%"} />
-      <Box>{children}</Box>
-    </Stack>
-  );
+    );
+
+  return <>{children}</>;
 }
