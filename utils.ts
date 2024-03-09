3ca17a9ec756c2b85a9b5c57e8d27a3686d08c20
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-09 21:10:27 +0900

    fix: fswitch function 이 불필요한 호출을 하지 않도록

--- utils.ts
+++ utils.ts
@@ -1,19 +1,14 @@
-import React from "react";
-
-interface Case {
-  when?: any;
-  factory: () => any;
-}
 export function fswitch(expression: any) {
+  const resolute = (value: any) => ({
+    case: (caseExpression: any, factory: () => any) => resolute(value).case,
+    default: () => value,
+  });
   const recursion = {
     case: function (caseExpression: any, factory: () => any) {
       if (expression === caseExpression) {
         const result = factory();
         if (result !== undefined) {
-          return Object.assign({}, factory(), {
-            case: recursion.case,
-            default: () => factory(),
-          });
+          return Object.assign({}, result, resolute(result));
         }
       }
       return recursion;
