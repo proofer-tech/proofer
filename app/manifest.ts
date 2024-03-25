ce7bf94fdae74ba78f17bfbb63a3504da1e029b4
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-25 22:45:02 +0900

    fix: Resource size is not correct - typo in the Manifest?

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,7 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/favicon.ico",
-        sizes: "any",
+        sizes: "48x48",
         type: "image/x-icon",
       },
       {
