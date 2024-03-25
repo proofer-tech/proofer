332f6d234fc6d690b25acfd8464fca5847c68115
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-25 22:49:48 +0900

    fix: Download error or resource isn't a valid image

--- app/manifest.ts
+++ app/manifest.ts
@@ -17,17 +17,17 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/x-icon",
       },
       {
-        src: "/icons/android-chrome-192x192.png",
+        src: "/pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/android-chrome-384x384.png",
-        sizes: "384x384",
-        type: "image/png",
-      },
-      {
-        src: "/icons/icon-512x512.png",
+        src: "/pwa-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
