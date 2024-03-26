7e4e4ffa3cce6c4f69d493fc882d3a47b1089feb
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-26 11:25:25 +0900

    debug: findWorkspace

--- app/subs/app/[workspaceSlug]/layout.tsx
+++ app/subs/app/[workspaceSlug]/layout.tsx
@@ -28,8 +28,9 @@ export default async function WorkspaceLayout({ children }: { children: any }) {
   if (!user) return redirect("/auth/login");
   if (!user?.email_verified) return redirect("/auth/email-verification");
 
+  console.log(pathname);
+  console.log(slug);
   const workspace = await findWorkspace(slug);
-  console.log(workspace);
   if (!workspace) return notFound();
 
   if (slug !== WORKSPACE_DEMO_SLUG) {
