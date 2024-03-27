e12e21640af32c75dca95ef08441063aef8117bb
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-27 16:00:36 +0900

    feat: 허전한 워크스페이스 메인화면 변경

--- app/subs/app/[workspaceSlug]/page.tsx
+++ app/subs/app/[workspaceSlug]/page.tsx
@@ -1,28 +1,47 @@
 "use client";
 import React, { useContext } from "react";
-import { Box, Group, Image, Stack, Text } from "@mantine/core";
+import { Box, Center, Group, Image, Stack, Text, Title } from "@mantine/core";
 import ProoferInsightContext from "@/app/subs/app/contexts/ProoferInsightContext";
+import { IconSquareArrowLeft } from "@tabler/icons-react";
 
 export default function Page() {
   const { workspace } = useContext(ProoferInsightContext);
 
   return (
-    <Group>
-      <Stack w={"30%"}>
-        <Group>
-          <Box>
-            {workspace?.instance.logoUrl && (
-              <Image
-                src={workspace?.instance.logoUrl}
-                alt={workspace?.instance.name}
-                height={"32px"}
-              />
-            )}
-          </Box>
-          <Text fw={700}>{workspace?.instance.name}</Text>
-        </Group>
-      </Stack>
-      <Stack></Stack>
-    </Group>
+    <Stack>
+      <Group>
+        <Stack w={"30%"}>
+          <Group>
+            <Box>
+              {workspace?.instance.logoUrl && (
+                <Image
+                  src={workspace?.instance.logoUrl}
+                  alt={workspace?.instance.name}
+                  height={"32px"}
+                />
+              )}
+            </Box>
+            <Text fw={700}>{workspace?.instance.name}</Text>
+          </Group>
+        </Stack>
+        <Stack></Stack>
+      </Group>
+      <Center h={"80vh"}>
+        <Stack align={"center"}>
+          <Image
+            src={"/assets/images/integrations.png"}
+            alt={"연동"}
+            w={"10em"}
+          />
+          <Title order={1} ta={"center"}>
+            우리 개발자의 생산성이 궁금하다면?
+          </Title>
+          <Group>
+            <IconSquareArrowLeft size={"1em"} />
+            <Text>좌측 메뉴를 클릭하여 다양한 인사이트들을 확인해보세요!</Text>
+          </Group>
+        </Stack>
+      </Center>
+    </Stack>
   );
 }
