ad3998da272f3b195a865c3599f9e5243645873a
Hansol-Lim <molmoty@gmail.com>
2024-04-03 10:17:46 +0900

    Add execute command

--- app/cron/execute-command/route.ts
+++ app/cron/execute-command/route.ts
@@ -15,28 +15,34 @@ export const GET = withCronApi(async function (req: NextRequest) {
     .from(Command)
     .where(eq(Command.state, CommandState.PENDING))) {
     await withLock({ id: `command:${command.hash}`, lease: 1000 }, async () => {
-      const serverlessFunctionPath = serverlessFunctionPathMap[command.name];
-      if (serverlessFunctionPath === undefined) {
+      try {
+        const serverlessFunctionPath = serverlessFunctionPathMap[command.name];
+        if (serverlessFunctionPath === undefined) {
+          await dz
+            .update(Command)
+            .set({ state: CommandState.FAILED })
+            .where(eq(Command.id, command.id));
+          console.error("Unknown command: ", command.hash);
+          return;
+        }
+
         await dz
           .update(Command)
-          .set({ state: CommandState.FAILED })
+          .set({ state: CommandState.PROCESSING })
           .where(eq(Command.id, command.id));
-        console.error("Unknown command: ", command.hash);
-        return;
-      }
 
-      await dz
-        .update(Command)
-        .set({ state: CommandState.PROCESSING })
-        .where(eq(Command.id, command.id));
-
-      const commandURL = new URL(serverlessFunctionPath, req.url);
-      for (const [k, v] of new URLSearchParams(
-        [`hash=${command.hash}`, command.arguments].join("&"),
-      )) {
-        commandURL.searchParams.set(k, v);
+        const commandURL = new URL(serverlessFunctionPath, req.url);
+        for (const [k, v] of new URLSearchParams(
+          [`hash=${command.hash}`, command.arguments].join("&"),
+        )) {
+          commandURL.searchParams.set(k, v);
+        }
+        fetch(commandURL);
+      } catch (e) {
+        const uuid = crypto.randomUUID();
+        console.error("Command error occurred: ", uuid);
+        console.log(uuid, e);
       }
-      fetch(commandURL);
     });
   }
   return NextResponse.json({});
