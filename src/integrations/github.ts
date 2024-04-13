475699a1822e3d05b35760a471b0d785c700f4ac
Hansol-Lim <molmoty@gmail.com>
2024-04-13 13:19:51 +0900

    GitHub OAuth App 을 위한 사전작업

--- src/integrations/github.ts
+++ src/integrations/github.ts
@@ -4,7 +4,7 @@ export const GitHubApp = new App({
   appId: process.env.GITHUB_APP_ID || "",
   privateKey: process.env.GITHUB_APP_PRIVATE_KEY || "",
   oauth: {
-    clientId: process.env.GITHUB_CLIENT_ID || "",
-    clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    clientId: process.env.GITHUB_APP_CLIENT_ID || "",
+    clientSecret: process.env.GITHUB_APP_CLIENT_SECRET || "",
   },
 });
