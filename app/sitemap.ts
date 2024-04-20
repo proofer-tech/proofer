1528d0170717c09f6c98751541e6daf40d1744b8
Hansol-Lim <molmoty@gmail.com>
2024-04-20 16:26:30 +0900

    fix: 중복 site 제거

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,18 +3,6 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.proofer.tech/docs/introduction-of-proofer",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.proofer.tech/subscribe",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
       url: "https://www.proofer.tech/subscribe",
       lastModified: new Date(),
       changeFrequency: "yearly",
@@ -31,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.4,
+    },
+    {
+      url: "https://www.proofer.tech/docs/introduction-of-proofer",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
