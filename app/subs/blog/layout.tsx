ec49effdd3fb2e5bd008fa3bee68727fe891c1fa
Hansol-Lim <molmoty@gmail.com>
2024-04-20 14:34:16 +0900

    metadata

--- app/subs/blog/layout.tsx
+++ app/subs/blog/layout.tsx
@@ -4,9 +4,10 @@ import { HeaderPortal } from "@/app/components/Header";
 import LandingPageShellLayout from "@/app/components/LandingPageShellLayout";
 import { get } from "@vercel/edge-config";
 import { generateMetadataFromTitle } from "@/src/manifest";
+import { SUB_DOMAIN, SUB_DOMAIN_NAMES } from "@/src/constants";
 
 export const metadata = generateMetadataFromTitle({
-  title: "프루퍼 테크",
+  title: `프루퍼 ${SUB_DOMAIN_NAMES[SUB_DOMAIN.blog]}`,
   description: "정확한 개발자 성과측정을 위한 엔지니어링 매니징 파트너",
 });
 export default async function Layout({ children }: any) {
