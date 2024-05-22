6643158a7260c21064557defcd16f60d9a61f01f
Hansol-Lim <molmoty@gmail.com>
2024-05-22 19:11:02 +0900

    참여링크 -> 링크

--- app/with-cto/CopyWithCTOButton.tsx
+++ app/with-cto/CopyWithCTOButton.tsx
@@ -8,7 +8,7 @@ export default function CopyWithCTOButton(props: CopyWithCTOButtonProps) {
     <CopyButton value="https://proofer.tech/with-cto">
       {({ copied, copy }) => (
         <Button color={copied ? "teal" : "blue"} onClick={copy} {...props}>
-          {copied ? "복사되었습니다!" : "참여링크 복사하여 공유하기"}
+          {copied ? "복사되었습니다!" : "링크 복사하여 공유하기"}
         </Button>
       )}
     </CopyButton>
