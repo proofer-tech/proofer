7efaf8e16928587314ff76020b907b87bbc3ceaa
Hansol-Lim <molmoty@gmail.com>
2024-05-22 18:57:54 +0900

    공유링크를 proofer tech 로 변경

--- app/with-cto/CopyWithCTOButton.tsx
+++ app/with-cto/CopyWithCTOButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 interface CopyWithCTOButtonProps extends ButtonProps {}
 export default function CopyWithCTOButton(props: CopyWithCTOButtonProps) {
   return (
-    <CopyButton value="https://bit.ly/with-cto">
+    <CopyButton value="https://proofer.tech/with-cto">
       {({ copied, copy }) => (
         <Button color={copied ? "teal" : "blue"} onClick={copy} {...props}>
           {copied ? "복사되었습니다!" : "참여링크 복사하여 공유하기"}
