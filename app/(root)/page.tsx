3ecd992d2d02878fd78b1cc5af964d0cfc18278c
Hansol-Lim <molmoty@gmail.com>
2024-05-26 11:14:19 +0900

    SEO 문구 수정

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
         <Stack gap={"xs"}>
           <Space h={"xl"} />
           <Text size={"md"} ta={"center"} c={"var(--mantine-color-gray-8)"}>
-            S.M.A.R.T 한 데이터 기반 성과관리
+            프루퍼, S.M.A.R.T 한 데이터 기반 성과관리
           </Text>
           <Text
             size={"xl"}
@@ -52,13 +52,13 @@ export default function Page() {
           <Space h={"1em"} />
           <Text size={"md"} ta={"center"} c={"var(--mantine-color-gray-6)"}>
             1on1, 설문조사, 다면평가와 같은 기존의 정성적인 방법으로만 측정한
-            성과는
+            성과만으로는
             <br />
             평가자마다 제각각인 기준으로 주관적이고 편견이 들어있을 수 있어
             업무성과를 정확히 추적하기에는 부족합니다.
             <br />
             이제는 프루퍼와 함께 실제 업무에 대한 풍부하고 다양한 신뢰성 높은
-            업무 데이터를 활용하여 &quot;진짜&quot; 성과를 알아보세요!
+            업무 데이터를 더하여 &quot;진짜&quot; 성과를 알아보세요!
           </Text>
           <Group justify={"center"} mt={"xl"}>
             <InquireForm withEmail={true} />
