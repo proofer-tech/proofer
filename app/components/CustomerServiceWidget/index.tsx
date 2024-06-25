182036dba0755c83dc789fcc47381ff721689bda
Hansol-Lim <molmoty@gmail.com>
2024-06-25 14:03:39 +0900

    하단 위젯추가

--- app/components/CustomerServiceWidget/index.tsx
+++ app/components/CustomerServiceWidget/index.tsx
@@ -1,29 +1,34 @@
 "use client";
 
-import { Affix, Group, Paper, Stack, Transition } from "@mantine/core";
+import { Affix, Flex, Group, Paper, Stack, Transition } from "@mantine/core";
 import { useMounted } from "@mantine/hooks";
 import { useIsMobileMedia } from "@/src/hooks/mediaQuery";
 import Image from "next/image";
 import { ReactNode } from "react";
+import stylesModule from "./styles.module.scss";
 
 interface CustomerServiceWidgetProps {
   buttons: ReactNode[];
   guidelines: ReactNode[];
+  position: {
+    top?: string | number;
+    left?: string | number;
+    bottom?: string | number;
+    right?: string | number;
+  };
+  onMobileClick?: () => void;
 }
 export default function CustomerServiceWidget({
+  position,
   buttons,
   guidelines,
+  onMobileClick,
 }: CustomerServiceWidgetProps) {
   const isMobileMedia = useIsMobileMedia();
   const isMounted = useMounted();
 
   return (
-    <Affix
-      position={{
-        bottom: isMobileMedia ? 12 : 60,
-        right: isMobileMedia ? 25 : 98,
-      }}
-    >
+    <Affix position={position}>
       <Transition transition="slide-up" mounted={isMounted}>
         {(styles) => (
           <Paper
@@ -31,13 +36,31 @@ export default function CustomerServiceWidget({
             bg={isMobileMedia ? "transparent" : "#F5F5FD"}
             radius={isMobileMedia ? 0 : 100}
             style={styles}
+            onClick={isMobileMedia && onMobileClick ? onMobileClick : () => {}}
+            className={
+              isMobileMedia && onMobileClick
+                ? stylesModule.mobileChatWidget
+                : ""
+            }
           >
-            <Group>
-              <Group gap={0} visibleFrom={"xs"}>
-                <Group>{...buttons}</Group>
-                <Stack gap={0} c={"var(--color-primary)"} py={"md"} px={"lg"}>
-                  {...guidelines}
-                </Stack>
+            <Flex
+              align={isMobileMedia ? "end" : "center"}
+              direction={isMobileMedia ? "column" : "row"}
+              wrap={"nowrap"}
+            >
+              <Group gap={0}>
+                <Group visibleFrom={"sm"}>{...buttons}</Group>
+                <Paper
+                  bg={isMobileMedia ? "#F5F5FD" : "transparent"}
+                  py={isMobileMedia ? "xs" : "md"}
+                  px={"lg"}
+                  radius={isMobileMedia ? 8 : 0}
+                  className={isMobileMedia ? stylesModule.speechBubble : ""}
+                >
+                  <Stack gap={0} c={"var(--color-primary)"}>
+                    {...guidelines}
+                  </Stack>
+                </Paper>
               </Group>
               <Image
                 src={"/assets/images/chat.webp"}
@@ -56,7 +79,7 @@ export default function CustomerServiceWidget({
                     : "calc(-1 * var(--mantine-spacing-lg) - 16px)",
                 }}
               />
-            </Group>
+            </Flex>
           </Paper>
         )}
       </Transition>
