4c26d9f1466f44daa65e388dbb721af16ee3d16d
Hansol-Lim <molmoty@gmail.com>
2024-07-11 19:03:41 +0900

    blog article 에 paper 추가

--- app/subs/blog/[...articlePath]/ArticlePage.tsx
+++ app/subs/blog/[...articlePath]/ArticlePage.tsx
@@ -3,6 +3,7 @@ import { Article, Tag } from "@/database/schemas/blog";
 import {
   Badge,
   Group,
+  Paper,
   Space,
   Stack,
   Title,
@@ -19,31 +20,33 @@ interface ArticleProps {
 
 export default function ArticlePage({ article }: ArticleProps) {
   return (
-    <Stack>
-      <Title order={1} fz={"xl"}>
-        {article.title}
-      </Title>
-      {article.tags.length > 0 ? (
-        <>
-          <Group gap={"xs"}>
-            {article.tags.map((tag) => (
-              <Badge key={tag.id} color="gray">
-                {tag.name}
-              </Badge>
-            ))}
-          </Group>
-          <Space h={"lg"} />
-        </>
-      ) : (
-        <></>
-      )}
-      <TypographyStylesProvider>
-        <div
-          className={styles.article}
-          // @ts-ignore
-          dangerouslySetInnerHTML={{ __html: article.contents }}
-        />
-      </TypographyStylesProvider>
-    </Stack>
+    <Paper p={"xl"} withBorder>
+      <Stack>
+        <Title order={1} fz={"xl"}>
+          {article.title}
+        </Title>
+        {article.tags.length > 0 ? (
+          <>
+            <Group gap={"xs"}>
+              {article.tags.map((tag) => (
+                <Badge key={tag.id} color="gray">
+                  {tag.name}
+                </Badge>
+              ))}
+            </Group>
+            <Space h={"lg"} />
+          </>
+        ) : (
+          <></>
+        )}
+        <TypographyStylesProvider>
+          <div
+            className={styles.article}
+            // @ts-ignore
+            dangerouslySetInnerHTML={{ __html: article.contents }}
+          />
+        </TypographyStylesProvider>
+      </Stack>
+    </Paper>
   );
 }
